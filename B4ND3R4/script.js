db233c2668fdaf3b3250989f2727c32c834fb8a8
Lasthearo <daniltkchn@gmail.com>
2022-04-15 04:16:49 +0300

    Moving!

--- B4ND3R4/script.js
+++ B4ND3R4/script.js
@@ -11,47 +11,80 @@ bg.src = "img/game-bg.png";
 obstacles.src = "img/bluecar.png";
 enemies.src = "img/enemie.png";
 
-enemies.onload = draw;
-
-// Moving player 
-var deltaX = 0;
-var deltaY = 0;
-window.addEventListener ("keydown", keysPressed, false);
-window.addEventListener ("keyup", keysReleased, false);
-var keys = [];
-function keysPressed(e) {
-
-// store an entry for every key pressed
-keys[e.keyCode] = true;
-
-    // left
-    if (keys[37]) {
-    deltaX -= 10;
-    }
-    // right 
-    if (keys[39]) {
-    deltaX += 10;
-    }
-    // down
-    if (keys[38]) {
-    deltaY -= 10; 
-    }
-    // up
-    if (keys[40]) {
-    deltaY += 10;
-    }
-    e.preventDefault();
-}
-    function keysReleased (e) {
-    // mark keys that were released
-        keys[e.keyCode] = false;
-    }
-
 // DRAW THE SCENE
 function draw() {
     ctx.drawImage(bg, 0, 0);
     ctx.drawImage(obstacles, 220, 40);
-    ctx.drawImage(player, deltaX, deltaY);
+    ctx.drawImage(player, velX, velY);
 }
+
+var x = 150,
+    y = 150,
+    velY = 0,
+    velX = 0,
+    speed = 6,
+    friction = 0.98,
+    keys = [];
+
+function update() {
+    requestAnimationFrame(update);
+    
+    if (keys[38]) {
+        if (velY > -speed) {
+            velY--;
+        }
+    }
+    
+    if (keys[40]) {
+        if (velY < speed) {
+            velY++;
+        }
+    }
+    if (keys[39]) {
+        if (velX < speed) {
+            velX++;
+        }
+    }
+    if (keys[37]) {
+        if (velX > -speed) {
+            velX--;
+        }
+    }
+
+    velY *= friction;
+    y += velY;
+    velX *= friction;
+    x += velX;
+
+    if (x >= 1155) {
+        x = 1155;
+    } else if (x <= 5) {
+        x = 5;
+    }
+
+    if (y > 715) {
+        y = 715;
+    } else if (y <= 5) {
+        y = 5;
+    }
+    
+    ctx.clearRect(0, 0, 1280, 720);
+    ctx.beginPath();
+    ctx.drawImage(player, x, y);
+    ctx.fill();
+}
+
+update();
+
+document.body.addEventListener("keydown", function (e) {
+    keys[e.keyCode] = true;
+});
+document.body.addEventListener("keyup", function (e) {
+    keys[e.keyCode] = false;
+});
+
+enemies.onload = draw;
+
+
 
 

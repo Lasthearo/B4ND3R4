7f8f29415fa792d939ecb32c58ab965665fe944f
Lasthearo <daniltkchn@gmail.com>
2022-04-15 14:42:58 +0300

    Imporvements

--- B4ND3R4/script.js
+++ B4ND3R4/script.js
@@ -1,3 +1,4 @@
+// GENERAL CODE
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 
@@ -10,20 +11,15 @@ player.src = "img/stepan-bandera.png";
 bg.src = "img/game-bg.png";
 obstacles.src = "img/bluecar.png";
 enemies.src = "img/enemie.png";
+// GENERAL CODE END
 
-// DRAW THE SCENE
-function draw() {
-    ctx.drawImage(bg, 0, 0);
-    ctx.drawImage(obstacles, 220, 40);
-    ctx.drawImage(player, velX, velY);
-}
-
+// PLAYER MOVING REALISATION
 var x = 150,
     y = 150,
     velY = 0,
     velX = 0,
-    speed = 6,
-    friction = 0.98,
+    speed = 10,
+    friction = 0.95,
     keys = [];
 
 function update() {
@@ -62,19 +58,17 @@ function update() {
         x = 5;
     }
 
-    if (y > 715) {
-        y = 715;
+    if (y > 650) {
+        y = 650;
     } else if (y <= 5) {
         y = 5;
     }
-    
-    ctx.clearRect(0, 0, 1280, 720);
-    ctx.beginPath();
-    ctx.drawImage(player, x, y);
-    ctx.fill();
-}
 
-update();
+    ctx.clearRect(0, 0, 1280, 720);
+    ctx.drawImage(bg, 0, 0);
+    ctx.drawImage(obstacles, 220, 40);
+    ctx.drawImage(player, x, y);
+}
 
 document.body.addEventListener("keydown", function (e) {
     keys[e.keyCode] = true;
@@ -82,9 +76,60 @@ document.body.addEventListener("keydown", function (e) {
 document.body.addEventListener("keyup", function (e) {
     keys[e.keyCode] = false;
 });
+// PLAYER MOVING REALISATION END
 
-enemies.onload = draw;
+// ROTATION COOL REALISATION START
+window.addEventListener('keydown', doKeyDown, true);
+window.addEventListener('keyup', doKeyUp, true);
 
+var keys = new Array();
+function doKeyDown(evt){
+    keys[evt.keyCode] = true;
+}
+function doKeyUp(evt){
+    keys[evt.keyCode] = false;
+}
+function convertToRadians(degree) {
+    return degree*(Math.PI/180);
+}
 
+function incrementAngle() {
+    angle += 10;
+    if(angle > 360) {
+        angle -= 360;
+    }
+}
+function decrementAngle(){
+    angle -= 10;
+    if(angle < 0){
+        angle += 360;
+    }
+}
 
+function drawRandomlyColoredRectangle() {  
+    if(90 in keys && keys[90]) { //left
+        decrementAngle();
+        console.log("left");
+    }
+    
+    if (88 in keys && keys[88]) { //right
+        //x += dx/5;
+        incrementAngle();
+        console.log("right");
+    }
+    
+    context.save();
+    context.clearRect(0,0,1280,720);
+    context.save();
+    context.translate(640,360);
+    context.rotate(convertToRadians(angle));
+    context.drawImage(player, -20, -25);
+    context.restore();
+}
+// ROTATION REALLY COOL REALISATION END
 
+var context = document.getElementById('canvas').getContext('2d');;
+var angle = 0;
+
+setInterval(drawRandomlyColoredRectangle, 20);
+update();

08b09d559590456c6e43df2cd24ea1da3a22bc0c
Lasthearo <daniltkchn@gmail.com>
2022-04-23 08:04:31 +0300

    Update

--- B4ND3R4/script.js
+++ B4ND3R4/script.js
@@ -19,26 +19,45 @@ enemies.src = "img/enemie.png";
 // GENERAL CODE END
 
 // PLAYER MOVING REALISATION
-var x = 150,
-    y = 150,
+var playerXpos = 150,
+    playerYpos = 150,
     velY = 15,
     velX = 10,
-    speed = 5,
+    speed = 7,
     friction = 0.95,
     keys = [];
 
-update();
-function update() {
-    requestAnimationFrame(update);
-    if(90 in keys && keys[90]) { //left
+function playerMoving() {
+    if(90 in keys && keys[90]) {
         decrementAngle();
-        console.log("left");
+        // console.log("left");
     }
     
-    if (88 in keys && keys[88]) { //right
-        //x += dx/5;
+    if (88 in keys && keys[88]) {
         incrementAngle();
-        console.log("right");
+        // console.log("right");
+    }
+
+    if (keys[32]) { //shoot
+        shooting = true;
+        console.log("shoot");
+    }
+
+    velY *= friction;
+    playerYpos += velY;
+    velX *= friction;
+    playerXpos += velX;
+
+    if (playerXpos >= cvs.width + 10) {
+        playerXpos = cvs.width + 10;
+    } else if (playerXpos <= 80) {
+        playerXpos = 80;
+    }
+
+    if (playerYpos > cvs.height + 10) {
+        playerYpos = cvs.height + 10;
+    } else if (playerYpos <= 80) {
+        playerYpos = 80;
     }
 
     if (keys[38]) {
@@ -63,69 +82,19 @@ function update() {
         }
     }
 
-    velY *= friction;
-    y += velY;
-    velX *= friction;
-    x += velX;
+    window.addEventListener('keydown', doKeyDown, true);
+    window.addEventListener('keyup', doKeyUp, true);
 
-    if (x >= cvs.width + 10) {
-        x = cvs.width + 10;
-    } else if (x <= 80) {
-        x = 80;
-    }
-
-    if (y > cvs.height + 10) {
-        y = cvs.height + 10;
-    } else if (y <= 80) {
-        y = 80;
-    }
-
-    ctx.save();
-    ctx.clearRect(0, 0, cvs.width, cvs.height);
-    ctx.drawImage(bg, 0, 0);
-    ctx.drawImage(obstacles, 220, 40);
-    ctx.translate(x - 50, y - 40);
-    ctx.drawImage(playerPlug, x, y);
-    ctx.rotate(convertToRadians(angle));
-    ctx.translate(-x - 50, -y - 40);
-    ctx.drawImage(player, x, y);
-    ctx.restore();
+    document.body.addEventListener("keydown", function (e) {
+        keys[e.keyCode] = true;
+    });
+    document.body.addEventListener("keyup", function (e) {
+        keys[e.keyCode] = false;
+    });
 }
 
-//SHOOTING REALISATION 
-function bullet(B){
-    B.width = 3;
-    B.height = 3;
-    B.color = "#000";
-    B.draw = function(){
-        canvas.fillStyle = this.color;
-        canvas.fillRect(this.x, this.y, this.width, this.height);
-    };
-    B.explode = function(){
-        setTimeout(function(){
-            B.active = true;
-        },100);
-    };
-    B.active = false;
-    return B;
-}
+// Rotation
 
-// 
-
-//SHOOTING REALISATION END
-
-window.addEventListener('keydown', doKeyDown, true);
-window.addEventListener('keyup', doKeyUp, true);
-document.body.addEventListener("keydown", function (e) {
-    keys[e.keyCode] = true;
-});
-document.body.addEventListener("keyup", function (e) {
-    keys[e.keyCode] = false;
-});
-// PLAYER MOVING REALISATION END
-
-// ROTATION COOL REALISATION START
-// setInterval(drawRandomlyColoredRectangle, 20);
 var keys = new Array();
 function doKeyDown(evt){
     keys[evt.keyCode] = true;
@@ -138,17 +107,100 @@ function convertToRadians(degree) {
 }
 
 function incrementAngle() {
-    angle += 5;
+    angle += 4;
     if(angle > 360) {
         angle -= 360;
     }
 }
 function decrementAngle(){
-    angle -= 5;
+    angle -= 4;
     if(angle < 0){
         angle += 360;
     }
 }
+
 var angle = 0;
-// ROTATION REALLY COOL REALISATION END
+
+// Rotation end
+// PLAYER MOVING REALISATION END
+
+//SHOOTING REALISATION
+var bulletXpos = 0;
+var bulletYpos = 0;
+const BULLET_WIDTH = 16;
+const BULLET_HEIGHT = 6;
+var bulletSpeed = 1.09;
+
+var shooting = false; 
+var shot = false;
+
+// document.addEventListener("keydown", KeyDown, false);
+// document.addEventListener("keyup", KeyUp, false);
+
+function bulletShoot() {
+    if(shooting && shot == false) {
+        bulletXpos = playerXpos + player.width - 30;
+        bulletYpos = playerYpos + player.height - 17;
+        shot = true;
+    }
+    if(shooting && shot) {
+        bulletXpos *= bulletSpeed;
+    }
+    if(bulletXpos < 0 || bulletXpos > cvs.width + 400) {
+        shot = false; 
+        shooting = false;
+    }
+    if(bulletYpos < 0 || bulletYpos > cvs.height) {
+        shot = false; 
+        shooting = false;
+    }
+    if(shot == false && shooting == false) {
+        bulletXpos = 0;
+        bulletYpos = 0;
+    }
+}
+//SHOOTING REALISATION END
+
+// GENERATING OBSTACLES
+
+// var obstX;
+// var obstY;
+// var bigObstWidth;
+// var bigObstHeight;
+
+// let bigObst = [
+//     {
+//         obstX : Math.random() * innerWidth,
+//         obstY : Math.random() * innerHeight,
+//     }
+
+// ]
+
+// GENERATING OBSTACLES END
+
+update();
+function update() {
+    requestAnimationFrame(update);
+
+    playerMoving();
+    bulletShoot();
+
+    ctx.save();
+    ctx.clearRect(0, 0, cvs.width, cvs.height);
+
+    ctx.drawImage(bg, 0, 0); // background
+    ctx.drawImage(obstacles, 220, 40); // obstacles
+    
+    ctx.translate(playerXpos - 50, playerYpos - 40);
+    ctx.drawImage(playerPlug, playerXpos, playerYpos);
+    ctx.rotate(convertToRadians(angle));
+    ctx.translate(-playerXpos - 50, -playerYpos - 40);
+
+    ctx.drawImage(player, playerXpos, playerYpos); // player
+    ctx.fillRect(bulletXpos, bulletYpos, BULLET_WIDTH, BULLET_HEIGHT); // bullet
+    ctx.restore();
+}
+
+
+
 
